2b29d0d8cbff2e096353813e21f26967fed53727
ujawall12 <ujawalldwivedi@gmail.com>
2023-09-02 11:28:43 +0530

    Spring framework basic-1

---
+++ src/main/java/com/springboot3/spring_framework/BeansAndConfigurations/App02HelloWorldSpring.java
@@ -0,0 +1,48 @@
+package com.springboot3.spring_framework.BeansAndConfigurations;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+    public static void main(String[] args) {
+
+        //1: Launch a Spring Context
+        try(var context =
+                    new AnnotationConfigApplicationContext
+                            (HelloWorldConfiguration.class)) {
+
+            //2: Configure the things that we want Spring to manage -
+            //HelloWorldConfiguration - @Configuration
+            //name - @Bean
+
+            //3: Retrieving Beans managed by Spring
+            System.out.println(context.getBean("name"));
+
+            System.out.println(context.getBean("age"));
+
+            System.out.println(context.getBean("person"));
+
+            System.out.println(context.getBean("person2MethodCall"));
+
+            System.out.println(context.getBean("person3Parameters"));
+
+            System.out.println(context.getBean("address2"));
+
+            System.out.println(context.getBean(Person.class));
+
+            System.out.println(context.getBean(Address.class));
+
+            System.out.println(context.getBean("person5Qualifier"));
+
+            // To get all the beans managed by Spring
+//			Arrays.stream(context.getBeanDefinitionNames())
+//				.forEach(System.out::println);
+
+        }
+
+
+
+
+    }
+
+}
---
+++ src/main/java/com/springboot3/spring_framework/Types_of_DependencyInjection/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,24 @@
+package com.springboot3.spring_framework.Types_of_DependencyInjection;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (SimpleSpringContextLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+        }
+    }
+}
